862152db55bc5f56d31802c97a1e78445ec27072
Nelapudi Sandeep <83591774+LearnToCodesandy@users.noreply.github.com>
2022-05-10 12:02:10 +0530

    commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,20 +1,33 @@
-import { useEffect } from 'react';
-import ContentContainer from '../../components/ContentContainer';
-import Navbar from '../../components/Navbar';
-import './home.css';
-import { getData } from '../../../functions';
+import { useEffect } from "react";
+import ContentContainer from "../../components/ContentContainer";
+import Navbar from "../../components/Navbar";
+import "./home.css";
+// import { getData } from "../../../functions";
 
 const Home = ({ setAuth }) => {
   useEffect(() => {
-    console.log('loaded!!!!');
     const data = {
-      category: 'movies',
-      language: 'kannada',
-      genre: 'all',
-      sort: 'voting',
+      category: "movies",
+      language: "kannada",
+      genre: "all",
+      sort: "voting"
     };
-    const promise = getData('https://hoblist.com/api/movieList', data);
-    Promise.all(promise).then((response) => console.log(response));
+    console.log(data);
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    };
+    async function getData() {
+      const response = await fetch("/api/movieList", options);
+      const data = response.json();
+      console.log(data);
+    }
+    getData();
+    // const promise = getData("/movieList", data);
+    // Promise.all(promise).then((response) => console.log(response));
   }, []);
   return (
     <div className="home__wrapper">
